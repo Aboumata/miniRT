6ffa430dcb72ac90af70cc6bc601fbc80ace1887
aboumata <fanchkhow@gmail.com>
2025-10-16 10:49:14 +0100

    add parsing files

--- miniRT.h
+++ miniRT.h
@@ -29,7 +29,7 @@ typedef struct s_vector3
     double z;
 } t_vector3;
 
-typedef struct s_color
+typedef struct s_colors
 {
     int r;
     int g;
@@ -43,19 +43,57 @@ typedef struct s_camera
     double    fov;
 } t_camera;
 
+typedef struct s_lights {
+    t_vector3 pos;
+    double ratio;
+    t_color color;
+    struct s_lights *next;
+} t_lights;
+
+typedef struct s_spheres {
+    t_vector3 center;
+    double diameter;
+    t_color color;
+    struct s_spheres *next;
+} t_spheres;
+
+typedef struct s_planes {
+    t_vector3 point;
+    t_vector3 normal;
+    t_color color;
+    struct s_planes *next;
+} t_planets;
+
 typedef struct s_ambient
 {
     double ratio;
     t_color color;
 } t_ambient;
 
+typedef struct s_cylinders {
+    t_vector3 center;
+    t_vector3 dir;
+    double diameter;
+    double hight;
+    t_color color;
+    struct s_cylinders *next;
+} t_cylinders;
+
 typedef struct s_scene
 {
     t_camera camera;
     t_ambient ambient;
+    t_lights *lights;
+    t_cylinders *cylinders;
+    t_planets *planets;
+    t_spheres *spheres;
+
+    int has_ambient;
+    int has_camera;
+    int has_light;
 } t_scene;
 
-void parsing(char *arg);
+void parsing(t_scene *scene, char *arg);
 
 #endif
 
