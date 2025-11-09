df2a3f74bd6d63f9de370f7ceac12b0089465cfd
aboumata <fanchkhow@gmail.com>
2025-11-09 06:12:25 +0100

    adjusted indentation and formatting in `ray.c` for improved code readability and consistency

--- srcs/ray.c
+++ srcs/ray.c
@@ -12,32 +12,31 @@
 
 #include "../includes/miniRT.h"
 
-t_vector3 ray_at(t_ray ray, double t)
+t_vector3	ray_at(t_ray ray, double t)
 {
-    return (vec_add(ray.origin, vec_scale(ray.direction, t)));
+	return (vec_add(ray.origin, vec_scale(ray.direction, t)));
 }
 
-t_ray create_ray(t_data *data, int x, int y)
+t_ray	create_ray(t_data *data, int x, int y)
 {
-    t_vector3 viewport_point;
-    t_vector3 horizontal_offset;
-    t_vector3 vertical_offset;
-    t_ray ray;
-    double u;
-    double v;
+	t_vector3	viewport_point;
+	t_vector3	horizontal_offset;
+	t_vector3	vertical_offset;
+	t_ray		ray;
+	double		u;
+	double		v;
 
-    u = (double)x / (double)WIDTH;
-    v = (double)y / (double)HEIGHT;
-    horizontal_offset = vec_scale(data->cam.right, u * data->cam.viewport_width);
-    vertical_offset = vec_scale(data->cam.up, -v * data->cam.viewport_height);
-    viewport_point = vec_add(data->cam.top_left, horizontal_offset);
-    viewport_point = vec_add(viewport_point, vertical_offset);
-
-    ray.origin = data->scene->camera.pos;
-    ray.direction = vec_sub(viewport_point, ray.origin);
-    ray.direction = vec_normalize(ray.direction);
-
-    return (ray);
+	u = (double)x / (double)WIDTH;
+	v = (double)y / (double)HEIGHT;
+	horizontal_offset = vec_scale(data->cam.right, u
+			* data->cam.viewport_width);
+	vertical_offset = vec_scale(data->cam.up, -v * data->cam.viewport_height);
+	viewport_point = vec_add(data->cam.top_left, horizontal_offset);
+	viewport_point = vec_add(viewport_point, vertical_offset);
+	ray.origin = data->scene->camera.pos;
+	ray.direction = vec_sub(viewport_point, ray.origin);
+	ray.direction = vec_normalize(ray.direction);
+	return (ray);
 }
 
 // void	print_ray_info(t_ray ray, int x, int y)
