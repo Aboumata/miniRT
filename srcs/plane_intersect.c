28ac1e140ee8a3c609b1cd27074218374c2fce33
aboumata <fanchkhow@gmail.com>
2025-11-14 11:47:59 +0100

    added `intersect_plane` function in `plane_intersect.c` to calculate ray-plane intersections; refactored plane normal logic into `plane_normal` for better modularity

--- srcs/plane_intersect.c
+++ srcs/plane_intersect.c
@@ -11,3 +11,53 @@
 /* ************************************************************************** */
 
 #include "../includes/miniRT.h"
+
+
+/*
+ * Ray: p(t) = origin + t * direction
+ * Plane: (P - plane_point) * normal = 0
+ *
+ * SOLVE!
+ *
+ * (origin + t * direction - plane_point) * normal = 0;
+ * normal * origin + t * (direction * normal) - plane_point * normal = 0
+ * t * (direction * normal) = plane_point * normal - normal * origin
+ * t * (direction * normal) = normal * (plane_point - origin)
+ * t = normal * (plane_point - origin) / (direction * normal)
+ *
+ */
+
+static t_vector3 plane_normal(t_planes *plane, t_ray ray)
+{
+    t_vector3 normal;
+
+    normal = plane->normal;
+    if (vec_dot(ray.direction , plane->normal) > 0)
+        normal = vec_scale(plane->normal, -1);
+    return (normal);
+}
+
+int intersect_plane(t_ray ray, t_planes *plane, t_hit *hit)
+{
+    double      denom;
+    double      numer;
+    double      t;
+    t_vector3   to_plane;
+    t_vector3   hit_point;
+    t_vector3   normal;
+
+    denom = vec_dot(ray.direction, plane->normal);
+    if (fabs(denom) < EPSILON)
+        return (0);
+    to_plane = vec_sub(plane->point, ray.origin);
+    numer = vec_dot(to_plane, plane->normal);
+    t = numer / denom;
+    if (!is_closer_hit(hit, t))
+        return (0);
+    hit_point = vec_add(ray.origin, vec_scale(ray.direction, t));
+    normal = plane_normal(plane, ray);
+    update_hit(hit, t, hit_point, normal, plane->color);
+    hit->object = plane;
+    hit->type = PL;
+    return (1);
+}
