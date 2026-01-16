005a6a79d0e6acb4260cac0c8d79fdcffe3dcb8f
dahmmani88 <abdelkebirdahmani@gmail.com>
2026-01-16 17:34:25 +0100

    checkerboard

--- srcs/plane_intersect.c
+++ srcs/plane_intersect.c
@@ -6,7 +6,7 @@
 /*   By: abdahman <abdahman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 21:33:36 by aboumata          #+#    #+#             */
-/*   Updated: 2026/01/15 16:43:07 by abdahman         ###   ########.fr       */
+/*   Updated: 2026/01/16 17:30:23 by abdahman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,6 @@ t_color	get_checkerboard_color(t_vector3 hit_p)
 	x = floor(hit_p.x) * size;
 	z = floor(hit_p.z) * size;
 	y = floor(hit_p.y) * size;
-	x = floor(hit_p.x * size);
-	z = floor(hit_p.z * size);
-	y = floor(hit_p.y * size);
 	if ((x + y + z) % 2 == 0)
 		return ((t_color){255, 255, 255});
 	else
@@ -54,7 +51,6 @@ static t_uv	get_plane_uv(t_vector3 p, t_planes *plane)
 		u_axis = vec_normalize(vec_cross((t_vector3){0, 1, 0}, n));
 	v_axis = vec_normalize(vec_cross(n, u_axis));
 	uv.u = vec_dot(vec_sub(p, plane->point), u_axis) * 0.1;
-	// uv.v = vec_dot(vec_sub(p, plane->point), v_axis) * 0.1;
 	uv.v = -vec_dot(vec_sub(p, plane->point), v_axis) * 0.1;
 	return (uv);
 }
@@ -82,14 +78,11 @@ int	intersect_plane(t_ray ray, t_planes *plane, t_hit *hit)
 	if (var.t < EPSILON || var.t > var.hit->t)
 		return (0);
 	var.hit_point = ray_at(ray, var.t);
-	// checkerboard(var.hit_point, plane);
 	final_color = plane->color;
 	if (plane->checkerboard)
 		final_color = get_checkerboard_color(var.hit_point);
-	// if (plane->bump_map)
 	if (plane->albedo_map || plane->bump_map)
 	{
-		// uv = plane_uv(var.hit_point, plane->point);
 		uv = get_plane_uv(var.hit_point, plane);
 		if (plane->albedo_map)
 			final_color = sample_texture_color(plane->albedo_map, uv);
@@ -98,7 +91,6 @@ int	intersect_plane(t_ray ray, t_planes *plane, t_hit *hit)
 	}
 	if (vec_dot(normal, ray.direction) > 0)
 		normal = vec_scale(normal, -1);
-	// update_hit(&var, normal, plane->color);
 	update_hit(&var, normal, final_color);
 	hit->normal = normal;
 	hit->object = plane;
